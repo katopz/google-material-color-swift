3ce51a30284937ffac2f4fbf231ad22255d5e6b3
katopz <katopz@gmail.com>
2014-12-20 16:35:37 +0700

    init

---
+++ DBKMaterialColor/ViewController.swift
@@ -0,0 +1,23 @@
+//
+//  ViewController.swift
+//  DBKMaterialColor
+//
+//  Created by Todsaporn Banjerdkit (katopz) on 12/19/14.
+//  Copyright (c) 2014 Debokeh. All rights reserved.
+//
+
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        self.view.backgroundColor = DBKMaterialColor.red500Color()
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+}
