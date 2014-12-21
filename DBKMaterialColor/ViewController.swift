a69ff3fffa3226d455234100076a30aab3737941
katopz <katopz@gmail.com>
2014-12-22 00:00:55 +0700

    add advance example which  change view background color randomly with animation

--- DBKMaterialColor/ViewController.swift
+++ DBKMaterialColor/ViewController.swift
@@ -9,15 +9,42 @@
 
 import UIKit
 
+// group of red color
+let REDS = [
+        DBKMaterialColor.red50Color(), DBKMaterialColor.red100Color(),
+        DBKMaterialColor.red200Color(), DBKMaterialColor.red300Color(),
+        DBKMaterialColor.red400Color(), DBKMaterialColor.red500Color(),
+        DBKMaterialColor.red600Color(), DBKMaterialColor.red700Color(),
+        DBKMaterialColor.red800Color(), DBKMaterialColor.red900Color(),
+        DBKMaterialColor.redA100Color(), DBKMaterialColor.redA200Color(),
+        DBKMaterialColor.redA400Color(), DBKMaterialColor.redA700Color()
+]
+
 class ViewController: UIViewController {
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        self.view.backgroundColor = DBKMaterialColor.red500Color()
+
+        // Basic : Set view background to Blue 500
+        self.view.backgroundColor = DBKMaterialColor.blue500Color()
+
+        // Advance : Change view background color randomly with animation
+        self.randomlyChangeViewBackgroudColorWithAnimation()
     }
 
-    override func didReceiveMemoryWarning() {
-        super.didReceiveMemoryWarning()
-        // Dispose of any resources that can be recreated.
+    func getRandomRed() -> UIColor {
+        return REDS[Int(arc4random_uniform(UInt32(REDS.count)))]
+    }
+
+    func randomlyChangeViewBackgroudColorWithAnimation() {
+        UIView.animateWithDuration(0.5, delay: 0.5, options: .CurveEaseOut,
+                animations: {
+                    self.view.backgroundColor = self.getRandomRed()
+                },
+                completion: {
+                    (complete: Bool) in
+                    self.randomlyChangeViewBackgroudColorWithAnimation();
+                    return
+                })
     }
 }
