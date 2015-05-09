9f12301262397d9bca910f8f9fc59f59850b558d
katopz <katopz@gmail.com>
2015-05-09 22:30:49 +0700

    add optional for alpha while init for extension

--- Source/GMColor.swift
+++ Source/GMColor.swift
@@ -9,13 +9,17 @@
 import UIKit
 
 extension UIColor {
-    convenience init(rgb: UInt) {
+    convenience init(rgb: UInt, a:CGFloat) {
         self.init(
-        red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
-                green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
-                blue: CGFloat(rgb & 0x0000FF) / 255.0,
-                alpha: CGFloat(1.0)
+            red: CGFloat((rgb & 0xFF0000) >> 16) / 255.0,
+            green: CGFloat((rgb & 0x00FF00) >> 8) / 255.0,
+            blue: CGFloat(rgb & 0x0000FF) / 255.0,
+            alpha: a
         )
+    }
+    
+    convenience init(rgb: UInt) {
+        self.init(rgb: rgb, a:1.0)
     }
 }
 
